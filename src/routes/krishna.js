f695b60b53ccacc9e7efef368542f3ab4fcde7f3
Sudhanshu <sudhanshu02512@gmail.com>
2025-12-25 17:52:28 +0530

    Added Delayer

--- src/routes/krishna.js
+++ src/routes/krishna.js
@@ -11,18 +11,31 @@ const pkg = getPackageInfo();
 const router = Router();
 
 /** Routing Configuration */
-router.get('/',(req,res)=>{
-    let response ={
-        status: 'up',
-        service: pkg.name,
-        version: pkg.version,
-        uptime: `${process.uptime().toFixed(2)} seconds`,
-        timestamp: new Date().toISOString(),
-        message: 'Service is running smoothly 🚀',
-        idol:'⭕‼️⭕',
-        support:"In case of any issue please contact sudhanshu02512@gmail.com"
-    }
-    res.send(response);
-})
+router.get("/", (req, res) => {
+  let response = {
+    status: "up",
+    service: pkg.name,
+    version: pkg.version,
+    uptime: `${process.uptime().toFixed(2)} seconds`,
+    timestamp: new Date().toISOString(),
+    message: "Service is running smoothly 🚀",
+    idol: "⭕‼️⭕",
+    support: "In case of any issue please contact sudhanshu02512@gmail.com",
+  };
+  res.send(response);
+});
+
+/**Delayer*/
+router.get("/delayer", (req, res) => {
+  const time = req.body?.time;
+  if (time) {
+    setTimeout(() => {
+      res.json({ message: `Response after ${time} ms` });
+    }, time); 
+  }
+  else{
+    res.json({ message: `Please provide time in ms in the request body` });
+  }
+});
 
 export default router;
